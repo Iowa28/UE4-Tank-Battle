4c78443ad12ba68c04fde45c6700a4015e2bb30e
naminov <naminov@tko-inform.ru>
2024-02-07 14:02:15 +0300

    create project

---
+++ UdemyProject2/Source/UdemyProject2/UdemyProject2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UdemyProject2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMYPROJECT2_API AUdemyProject2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
