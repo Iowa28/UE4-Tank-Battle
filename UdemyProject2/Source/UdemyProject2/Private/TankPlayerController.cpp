046e3372e3d227d67d36cff377fc5a0b1cc47eea
naminov <naminov@tko-inform.ru>
2024-02-09 20:19:46 +0300

    camera look location

--- UdemyProject2/Source/UdemyProject2/Private/TankPlayerController.cpp
+++ UdemyProject2/Source/UdemyProject2/Private/TankPlayerController.cpp
@@ -3,22 +3,69 @@
 
 #include "UdemyProject2/Public/TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
-void ATankPlayerController::BeginPlay()
+void ATankPlayerController::Tick(float DeltaSeconds)
 {
-	Super::BeginPlay();
+	Super::Tick(DeltaSeconds);
+	AimTowardsCrosshair();
+}
 
-	// ATank* Tank = GetControlledTank();
-	// if (Tank)
-	// {
-	// 	UE_LOG(LogTemp, Warning, TEXT("Tank: %s"), *Tank->GetName());
-	// }
-	// else
-	// {
-	// 	UE_LOG(LogTemp, Warning, TEXT("Missing tank"));
-	// }
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		return;
+	}
+
+	FVector HitLocation;
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *HitLocation.ToString());
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		return GetLookVectorHitLocation(LookDirection, HitLocation);
+	}
+	
+	return false;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector WorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLocation, LookDirection);
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	FVector StartLocation = PlayerCameraManager->GetCameraLocation();
+	FVector EndLocation = StartLocation + LookDirection * LineTraceRange;
+	
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility))
+	{
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	HitLocation = FVector::ZeroVector;
+
+	return false;
 }
--- UdemyProject2/Source/UdemyProject2/Public/TankPlayerController.h
+++ UdemyProject2/Source/UdemyProject2/Public/TankPlayerController.h
@@ -15,9 +15,29 @@ class UDEMYPROJECT2_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public:
-	ATank* GetControlledTank() const;
-
 protected:
 	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = .5;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = .3333;
+
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
+	
+	ATank* GetControlledTank() const;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 };
